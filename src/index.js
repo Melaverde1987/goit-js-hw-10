99400f4d344140126d37f965da18d170549c45cc
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-08-28 21:07:05 +0200

    styling

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_maob73XQNST9k0E0qcYB5rumD7MyvNnpZXqqaHnME5AqsJSKmgk7yDm4Yo3HmZXZ';
 
 const select = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
 const info = document.querySelector('.cat-info');
 
 function handlerBreedSearch() {
   fetchBreeds()
     .then(({ data }) => {
       select.innerHTML = createSelectMarkup(data);
+      new SlimSelect({
+        select: select,
+        settings: {
+          showSearch: false,
+        },
+      });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      Notify.failure(err.message);
+    })
+    .finally(() => {
+      loader.classList.add('hidden');
+    });
 }
 
 handlerBreedSearch();
@@ -26,21 +41,33 @@ function createSelectMarkup(arr) {
 select.addEventListener('change', handlerSelect);
 
 function handlerSelect(evt) {
+  loader.classList.remove('hidden');
+
   fetchCatByBreed(evt.currentTarget.value)
-    .then(({ data }) => (info.innerHTML = createMarkup(data)))
-    .catch(err => console.log(err));
+    .then(({ data }) => {
+      Notify.success('Meow!');
+      info.innerHTML = createMarkup(data);
+    })
+    .catch(err => {
+      Notify.failure(err.message);
+    })
+    .finally(() => {
+      loader.classList.add('hidden');
+    });
 }
 
 function createMarkup(arr) {
   return arr
     .map(
       ({ breeds, url }) => `
-        <img src="${url}" alt="${breeds.map(item => item.name)}"/>
-        <h2 class="name">${breeds.map(item => item.name)}</h2>
-        <div class="description">${breeds.map(item => item.description)}</div>
-        <div class="temperament"><span>Temperament: </span>${breeds.map(
-          item => item.temperament
-        )}</div>
+        <img src="${url}" class="image" alt="${breeds.map(item => item.name)}"/>
+        <div class="text">
+          <h2 class="name">${breeds.map(item => item.name)}</h2>
+          <div class="temperament"><span>Temperament: </span>${breeds.map(
+            item => item.temperament
+          )}</div>
+          <div class="description">${breeds.map(item => item.description)}</div>
+        </div>
     `
     )
     .join('');
